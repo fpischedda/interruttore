d34bfcec9372e33acad709219e37c9bcae14ba44
foca <francesco@pischedda.info>
2020-04-10 07:11:13 +0200

    unit testing internal api

--- src/circuit_breaker/core.clj
+++ src/circuit_breaker/core.clj
@@ -1,38 +1,84 @@
 (ns circuit-breaker.core
   (:import
-   [java.time ZoneOffset LocalDate LocalDateTime]))
+   [java.time Duration LocalDateTime ZoneOffset]))
 
-(defn- circuit-next-state [{:keys [state unlock-after]}]
-  (cond
-    (and (= :open state) (.IsBefore unlock-after (LocalDateTime/now ZoneOffset/UTC))) :semi-open
-    (= :semi-open state) :closed
-    :else state))
-
-(defn- tomorrow
+(defn now
   []
-  (.atStartOfDay (.plusDays (LocalDate/now ZoneOffset/UTC) 1)))
+  (LocalDateTime/now ZoneOffset/UTC))
 
-(defn- next-month
-  []
-  (.atStartOfDay (.with (.plusMonths (LocalDate/now ZoneOffset/UTC) 1)
-                   (TemporalAdjusters/firstDayOfMonth))))
+(defn circuit-open?
+  "Tells if the circuit is open or not; having retry-after set to nil
+   means that the circuit will be open until reset."
+  [{::keys [status retry-after]}]
+  (and (= ::open status)
+    (or (nil? retry-after) (.isBefore (now) retry-after))))
 
-(defn- apply-soft-failure
-  [circuit]
-  (let [retry (inc (:retry-counter circuit))
-        new-state (if (or (= :semi-open (:state circuit))
-                        (>= retry max-retries))
-                    :open
-                    :closed)
-        after (if (= :closed new-state)
-                (compute-next-retry next-retry)
-                nil)]
-    {:retry-counter retry
-     :unlock-after after
-     :state new-state}))
+(comment
+  (circuit-open? {::status ::closed}) ;; => false
+  (circuit-open? {::status ::semi-open}) ;; => false
+  (circuit-open? {::status ::open}) ;; => true
+  (circuit-open? {::status ::open
+                  ::retry-after (.plus (now) (Duration/ofMinutes 1))}) ;; => true
+  (circuit-open? {::status ::open
+                  ::retry-after (.minus (now) (Duration/ofMinutes 1))}) ;; => false
+  )
 
-(defn mk-circuit-breaker
-  "Return a fn wrapping the provided body with a circuit breaker
+(defn- circuit-next-state
+  "Compute the next state of the circuit based on the result of the call
+  to the wrapped function (`wfn`) which sholud have the following schema:
+  {:status (s/enum :ok :soft-failure :hard-failure)
+   (s/optional-key :value) s/Any}
+  depending on the value of the :status key one of the following things
+  will happen:
+  - :ok : set the circuit state to :closed and return the value of :value
+  - :soft-failure : retry calling `wfn` at most max-retries times until:
+    - a valid response is returned, then proceed as the :ok case
+    - after max-retries times put the circuit in :open state, set
+      :retry-after timestamp to control when to try again, and return
+      an error
+  - :hard-failure : skip :soft-failure machinery, set the circuit to
+    :open state, set :retry-after timestamp and return an error
+  "
+  [{::keys [status retry-count retry-after-ms max-retries] :as circuit
+    :or    {retry-count 0
+            max-retries 3}}
+   ;; aliasing _result just for documentation
+   {:keys [result retry-after] :as _result}]
+  (println "retry count" retry-count)
+  (println circuit)
+  (case result
+    :ok ;; everything is fine this time!
+    ;; but maybe it was not :ok before, so be careful
+    (assoc circuit
+      ::status (if (= ::open status) ::semi-open ::closed)
+      ::retry-after nil
+      ::retry-count (if (= ::open status) retry-count 0))
+
+    ;; in case of soft failure check how many times we have alredy RE-tried
+    ;; to decide if the circuit have to be opened or if we can retry again
+    :soft-failure
+    (assoc circuit
+      ::status (if (>= retry-count max-retries) ::open ::closed)
+      ::retry-count (inc retry-count)
+      ::retry-after (when (>= retry-count max-retries)
+                     (or retry-after retry-after-ms)))
+
+    ;; hard failure, STOP THE WORLD!!!
+    ;; setting retry-count to max-retries to support semi-open case
+    :hard-failure
+    (assoc circuit
+      ::status ::open
+      ::retry-count max-retries
+      ::retry-after (or retry-after retry-after-ms))))
+
+(comment
+  (circuit-next-state {::status ::closed} {:result :ok})
+  (circuit-next-state {::status ::closed} {:result :soft-failure})
+  )
+
+(defn make-circuit-breaker
+  "Return a fn wrapping the provided wrapped-fn with a circuit breaker.
+
   The wrapped function (wfn) is expected to return a map with the
   following schema:
   {:status (s/enum :ok :soft-failure :hard-failure)
@@ -46,40 +92,66 @@
       :retry-after timestamp to control when to try again, and return
       an error
   - :hard-failure : skip :soft-failure machinery, set the circuit to
-    :open state, set :retry-after timestamo and return an error
+    :open state, set :retry-after timestamp and return an error
 
   in the case `wfn` throws the :soft-failure case will be used
   "
-  [{:keys [body-fn state-transition-fn max-retries next-retry]
+  [{:keys [wrapped-fn max-retries retry-after-ms]
     :or
     {max-retries 3
-     next-retry [:ms 3]}}]
-  (let [circuit_ (atom {:retry-counter 0
+     retry-after-ms 10}}]
+  (let [circuit_ (atom {:retry-count 0
                         :retry-after nil
-                        :state :closed})])
-  (fn [& args]
-    (swap! circuit_ #(-> % circuit-next-state (assoc % :state)))
-    (when-not (= :open (:state @circuit_))
-      (try
-        (let [res (apply body-fn args)]
-          (cond))
-        (catch Throwable t
-          (swap! circuit_
-            (fn [circuit]
-              (let [retry (inc (:retry-counter circuit))
-                    new-state (if (or (= :semi-open (:state circuit))
-                                    (>= retry max-retries))
-                                :open
-                                :closed)
-                    after (if (= :closed new-state)
-                            (compute-next-retry next-retry)
-                            nil)]
-                {:retry-counter retry
-                 :unlock-after after
-                 :state new-state})))
-          (if (= :closed (:state @circuit_))
-            (do
-              (Thread/sleep (* 3 (:retry-counter @circuit_)))
-              (recur))
-            {:status :open
-             :reason :max-retries}))))))
+                        :retry-after-ms retry-after-ms
+                        :max-retries max-retries
+                        :status :closed})]
+    (with-meta
+      (fn [& args]
+        (if (circuit-open? @circuit_)
+          ;; circuit still open, fail early
+          {:status :open
+           :retry-after (:retry-after @circuit_)}
+          ;; circuit not open, try to call wrapped-fn and handle the result
+          (loop []
+            (let [{:keys [result value] :as res}
+                  (try
+                    (apply wrapped-fn args)
+                    (catch Throwable t
+                      ;; exception are considered as soft-failure
+                      {:result :soft-failure}))]
+              ;; evaluate result and prev state to calculate next state
+              (swap! circuit_ #(circuit-next-state % res))
+
+              (let [{:keys [status retry-count retry-after]} @circuit_]
+                (cond
+                  (= :open status)
+                  ;; wrapped-fn failed too many times
+                  {:status :open
+                   :retry-after retry-after}
+
+                  ;; wrapped-fn did not return ok, sleep a bit and retry
+                  (not (= :ok result))
+                  (do
+                    (Thread/sleep (* retry-after-ms (inc retry-count)))
+                    (recur))
+
+                  ;; wrapped-fn was successful, status can be :closed
+                  ;; or :semi-open
+                  :else
+                  {:status status
+                   :value value}
+                  ))))))
+      {:circuit_ circuit_})))
+
+(defn inspect
+  "Return the status of the provided circuit (as an atom)"
+  [c]
+  (meta c :circuit_))
+
+(defn reset
+  "Reset the circuit to initial state"
+  [c]
+  (swap! (meta c :circuit_) assoc
+    :status :closed
+    :retry-count 0
+    :retry-after nil))
--- test/circuit_breaker/core_test.clj
+++ test/circuit_breaker/core_test.clj
@@ -1,8 +1,78 @@
 (ns circuit-breaker.core-test
   (:require [clojure.test :refer :all]
-            [circuit-breaker.core :refer :all]))
+            [circuit-breaker.core :as cb])
+  (:import
+   [java.time Duration LocalDateTime ZoneOffset]))
 
+(defn now
+  []
+  (LocalDateTime/now ZoneOffset/UTC))
 
-(deftest a-test
-  (testing "FIXME, I don't fail."
-    (is (true? (= 4 (count (str (= 1 1))))))))
+(deftest circuit-open?
+  (testing "Circuit not open when ::closed :)"
+    (is (false? (cb/circuit-open? {::status ::closed}))))
+  (testing "Circuit not open when ::semi-open"
+    (is (false? (cb/circuit-open? {::status ::semi-open}))))
+  (testing "Circuit not open when ::status ::open and ::retry-after is expired"
+    (is (false? (cb/circuit-open?
+                  {::cb/status ::cb/open
+                   ::cb/retry-after (.minus (now) (Duration/ofMinutes 1))}))))
+  (testing "Circuit open when ::status ::open and ::retry-after not expired"
+    (is (true? (cb/circuit-open?
+                 {::cb/status ::cb/open
+                  ::cb/retry-after (.plus (now) (Duration/ofMinutes 1))})))))
+
+(deftest circuit-next-state
+  (testing "Circuit ::closed, result is :ok, circtuit stays ::closed"
+    (is (= {::cb/status ::cb/closed
+            ::cb/retry-after nil
+            ::cb/retry-count 0}
+          (#'cb/circuit-next-state {::cb/status ::cb/closed} {:result :ok}))))
+  (testing "Circuit ::closed, first :soft-failure, inc ::retry-count to 1"
+    (is (= {::cb/status ::cb/closed
+            ::cb/retry-after nil
+            ::cb/retry-count 1}
+          (#'cb/circuit-next-state {::cb/status ::cb/closed} {:result :soft-failure}))))
+  (testing "Circuit ::closed, last :soft-failure, set to ::open"
+    (is (= {::cb/status ::cb/open
+            ::cb/retry-after 3
+            ::cb/max-retries 1
+            ::cb/retry-count 2}
+          (#'cb/circuit-next-state
+            {::cb/status ::cb/closed
+             ::cb/max-retries 1
+             ::cb/retry-count 1}
+            {:result :soft-failure
+             :retry-after 3}))))
+  (testing "Circuit ::open, result :ok, set to ::semi-open"
+    (is (= {::cb/status ::cb/semi-open
+            ::cb/retry-after nil
+            ::cb/max-retries 1
+            ::cb/retry-count 1}
+          (#'cb/circuit-next-state
+            {::cb/status ::cb/open
+             ::cb/max-retries 1
+             ::cb/retry-count 1}
+            {:result :ok}))))
+  (testing "Circuit ::semi-open, result :ok, set to ::closed"
+    (is (= {::cb/status ::cb/closed
+            ::cb/retry-after nil
+            ::cb/max-retries 1
+            ::cb/retry-count 0}
+          (#'cb/circuit-next-state
+            {::cb/status ::cb/semi-open
+             ::cb/max-retries 1
+             ::cb/retry-count 1}
+            {:result :ok}))))
+  (testing "Circuit ::closed, first :hard-failure, set to ::open"
+    (is (= {::cb/status ::cb/open
+            ::cb/retry-after 5
+            ::cb/max-retries 3
+            ::cb/retry-count 3}
+          (#'cb/circuit-next-state
+            {::cb/status ::cb/closed
+             ::cb/max-retries 3
+             ::cb/retry-count 1}
+            {:result :hard-failure
+             :retry-after 5}))))
+  )
